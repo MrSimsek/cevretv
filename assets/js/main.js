d4846f25d17a721ee5879d44c7447e5ddb6fd07e
mrsimsek <denizsimsek93@gmail.com>
2017-11-01 22:54:57 +0300

    Major UI updates
    Change landing screen, shows, about, contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,11 @@
 window.onload = function(){ 
-    // Get the modal
+
     var modal = document.getElementById('show-modal');
     var modalVideo = document.getElementById("modal-video");
 
-    /*
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var show = document.getElementById('show-1');
-    show.onclick = function(){
-        modal.style.display = "block";
-    }
-    */
-
     function openModal() {
-        modal.style.display = "block";
         modalVideo.src = this.getAttribute("data-video");
+        window.scrollTo(0, 0);
     }
 
     var shows = document.querySelectorAll('.show');
@@ -21,12 +13,10 @@ window.onload = function(){
         shows[i].addEventListener('click', openModal);
     }
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    /* var span = document.getElementsByClassName("close")[0];
     
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
         modal.style.display = "none";
         modalVideo.src = "";
-    } 
+    }  */
 };
